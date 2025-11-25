09f94afd89c68601ce79b7f8360a298b48fe84ee
wukoo <register09@126.com>
2025-11-25 02:19:27 +0000

    update

--- exercises/tests/tests4.rs
+++ exercises/tests/tests4.rs
@@ -5,7 +5,6 @@
 // Execute `rustlings hint tests4` or use the `hint` watch subcommand for a
 // hint.
 
-
 struct Rectangle {
     width: i32,
     height: i32
@@ -34,14 +33,14 @@ mod tests {
     }
 
     #[test]
-    #[should_panic(expected = "Rectangle width and height cannot be negative!")]
+    #[should_panic]
     fn negative_width() {
         // This test should check if program panics when we try to create rectangle with negative width
         let _rect = Rectangle::new(-10, 10);
     }
 
     #[test]
-    #[should_panic()]
+    #[should_panic]
     fn negative_height() {
         // This test should check if program panics when we try to create rectangle with negative height
         let _rect = Rectangle::new(10, -10);
